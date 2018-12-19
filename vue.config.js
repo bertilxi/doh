46d763fb33fccd4b7dc79ff4eccdec4379ab79f1
Fernando Berti <bertilxi@gmail.com>
2018-12-19 00:01:32 -0300

    fixed icons and manifest

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,11 @@
 module.exports = {
   pwa: {
-    name: 'Doh',
-    themeColor: '#FFD100',
-    msTileColor: '#FFFFFF'
+    name: "Doh",
+    themeColor: "#ffcf00",
+    msTileColor: "#FFFFFF",
+    favicon32: "images/icons/favicon-72x72.png",
+    favicon16: "images/icons/favicon-72x72.png",
+    appleTouchIcon: "images/icons/icon-152x152.png",
+    msTileImage: "images/icons/icon-144x144.png"
   }
-}
+};
