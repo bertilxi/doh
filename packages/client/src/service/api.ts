617cc2d3837d90d39b605f4a5eb95f2e2f82df92
Fernando Berti <bertilxi@gmail.com>
2018-12-30 18:29:15 -0300

    fixed https

--- packages/client/src/service/api.ts
+++ packages/client/src/service/api.ts
@@ -5,7 +5,10 @@ import {
 } from "axios-extensions";
 
 export const api = axios.create({
-  baseURL: "http://localhost:8080/api",
+  baseURL:
+    process.env.NODE_ENV === "production"
+      ? "https://localhost/api"
+      : "http://localhost:8080/api",
   headers: { "Cache-Control": "no-cache" },
   adapter: throttleAdapterEnhancer(
     cacheAdapterEnhancer(axios.defaults.adapter!)
